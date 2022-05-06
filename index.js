18f05f80ae4ca0a8e595fa7667b561e77cfab558
Franziska-F <f.fouchs@gmail.com>
2022-05-06 16:05:54 +0200

    Delete outdated code

--- index.js
+++ index.js
@@ -1,46 +1,6 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-// let color = randomColor();
-
-// let hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-
-/* function getHex() {
-  let hexColor = '#';
-  for (let i = 0; i < 6; i++) {
-    hexColor += hex[Math.floor(Math.random() * 16)];
-    console.log(hexColor);
-  }
-}
-getHex(); 
-
-// get a random color: 
-function repeatHashtag() {
-  console.log(chalk.hex(color)('#'.repeat(31)));
-}
-for (let i = 0; i < 9; i++) {
-  repeatHashtag();
-}
-// get input 
-function repeatHashtag() {
-  console.log(chalk.(process.argv[2])('#'.repeat(31)));
-}
-for (let i = 0; i < 9; i++) {
-  repeatHashtag();}
-
-// in one function
-
-function repeatHashtag() {
-  if (process.argv.length < 3) {
-    console.log(chalk.hex(color)('#'.repeat(31)));
-  } else {
-    console.log(chalk.blue)('#'.repeat(31));
-  }
-  for (let i = 0; i < 9; i++) {
-    repeatHashtag();
-  } 
-} */
-
 let color = randomColor({
   hue: process.argv[2],
   luminosity: process.argv[3],
